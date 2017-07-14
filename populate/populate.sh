ffaed499489677319bd6cbb281bafd15e403e06a
Charles <xuchi.int@gmail.com>
2017-07-13 17:27:39 -0700

    update

--- populate/populate.sh
+++ populate/populate.sh
@@ -20,15 +20,15 @@ if [ ! -d "/home/chix/kvm/image" ]; then
 fi
 if [ ! -d "/home/chix/kvm/linux-bridge-start" ]; then
   mkdir -p /home/chix/kvm/linux-bridge-start
-  echo "#!/bin/bash\n" > /home/chix/kvm/linux-bridge-start/start-vm-all.sh
+  echo "#!/bin/bash" > /home/chix/kvm/linux-bridge-start/start-vm-all.sh
 fi
 if [ ! -d "/home/chix/kvm/ovs-dpdk-start" ]; then
   mkdir -p /home/chix/kvm/ovs-dpdk-start
-  echo "#!/bin/bash\n" > /home/chix/kvm/ovs-dpdk-start/start-vm-all.sh
+  echo "#!/bin/bash" > /home/chix/kvm/ovs-dpdk-start/start-vm-all.sh
 fi
 if [ ! -d "/home/chix/kvm/ovs-nodpdk-start" ]; then
   mkdir -p /home/chix/kvm/ovs-nodpdk-start
-  echo "#!/bin/bash\n" > /home/chix/kvm/ovs-nodpdk-start/start-vm-all.sh
+  echo "#!/bin/bash" > /home/chix/kvm/ovs-nodpdk-start/start-vm-all.sh
 fi
 if [ ! -d "/mnt/kvm-image" ]; then
   mkdir -p /mnt/kvm-image
@@ -43,41 +43,41 @@ do
   
   echo "start to copy kvm ovs dpdk scripts for VM $cur"
 
-	echo "sudo /home/chix/qemu-2.9.0/x86_64-softmmu/qemu-system-x86_64 -vnc :$cur \\\n
-		-enable-kvm -smp 8 -m 16384 \\\n
-		-drive file=$KVMDIR/image/image-$cur.img,if=virtio \\\n
-		-object memory-backend-file,id=mem,size=16384M,mem-path=/dev/hugepages,share=on \\\n
-		-numa node,memdev=mem -mem-prealloc \\\n
-		-chardev socket,id=char1,path=$DPDKDIR/socket/vhost-client-$cur-1,server \\\n
-		-netdev type=vhost-user,id=mynet1,chardev=char1,vhostforce \\\n
-		-device virtio-net-pci,mac=00:00:00:00:10:$cur,netdev=mynet1 \\\n
-		-chardev socket,id=char2,path=$DPDKDIR/socket/vhost-client-$cur-2,server \\\n
-		-netdev type=vhost-user,id=mynet2,chardev=char2,vhostforce \\\n
-		-device virtio-net-pci,mac=00:00:00:00:11:$cur,netdev=mynet2 \\\n" > $KVMDIR/ovs-dpdk-start/$cur.sh
+	echo "sudo /home/chix/qemu-2.9.0/x86_64-softmmu/qemu-system-x86_64 -vnc :$cur \\
+		-enable-kvm -smp 8 -m 16384 \\
+		-drive file=$KVMDIR/image/image-$cur.img,if=virtio \\
+		-object memory-backend-file,id=mem,size=16384M,mem-path=/dev/hugepages,share=on \\
+		-numa node,memdev=mem -mem-prealloc \\
+		-chardev socket,id=char1,path=$DPDKDIR/socket/vhost-client-$cur-1,server \\
+		-netdev type=vhost-user,id=mynet1,chardev=char1,vhostforce \\
+		-device virtio-net-pci,mac=00:00:00:00:10:$cur,netdev=mynet1 \\
+		-chardev socket,id=char2,path=$DPDKDIR/socket/vhost-client-$cur-2,server \\
+		-netdev type=vhost-user,id=mynet2,chardev=char2,vhostforce \\
+		-device virtio-net-pci,mac=00:00:00:00:11:$cur,netdev=mynet2" > $KVMDIR/ovs-dpdk-start/$cur.sh
 
 
   echo "start to copy kvm ovs nodpdk scripts for VM $cur"
 
-	 echo " sudo qemu-system-x86_64 -vnc :$cur \\\n
-		-enable-kvm -smp 8 -m 16384 \\\n
-		-drive file=$KVMDIR/image/image-$cur.img,if=virtio \\\n
-		-net nic,macaddr=00:00:00:00:10:$cur,vlan=1 \\\n
-		-net tap,vhost=on,id=vnic1,script=$KVMDIR/ovs-if-script/ovs-ifup1,downscript=$KVMDIR/ovs-if-script/ovs-ifdown1,vlan=1 \\\n
-		-net nic,macaddr=00:00:00:00:11:$cur,vlan=2 \\\n
-		-net tap,vhost=on,id=vnic2,script=$KVMDIR/ovs-if-script/ovs-ifup2,downscript=$KVMDIR/ovs-if-script/ovs-ifdown2,vlan=2 \\\n
-		-display sdl -vga std \\" > $KVMDIR/ovs-nodpdk-start/$cur.sh
+	 echo " sudo qemu-system-x86_64 -vnc :$cur \\
+		-enable-kvm -smp 8 -m 16384 \\
+		-drive file=$KVMDIR/image/image-$cur.img,if=virtio \\
+		-net nic,macaddr=00:00:00:00:10:$cur,vlan=1 \\
+		-net tap,vhost=on,id=vnic1,script=$KVMDIR/ovs-if-script/ovs-ifup1,downscript=$KVMDIR/ovs-if-script/ovs-ifdown1,vlan=1 \\
+		-net nic,macaddr=00:00:00:00:11:$cur,vlan=2 \\
+		-net tap,vhost=on,id=vnic2,script=$KVMDIR/ovs-if-script/ovs-ifup2,downscript=$KVMDIR/ovs-if-script/ovs-ifdown2,vlan=2 \\
+		-display sdl -vga std" > $KVMDIR/ovs-nodpdk-start/$cur.sh
 
 
   echo "start to copy kvm linux bridge scripts for VM $cur"
 
-	  echo " sudo qemu-system-x86_64 -vnc :$cur \\\n
-		-enable-kvm -smp 8 -m 16384 \\\n
-		-drive file=$KVMDIR/image/image-$cur.img,if=virtio \\\n
-		-net nic,macaddr=00:00:00:00:10:$cur,vlan=1 \\\n
-		-net tap,vhost=on,id=vnic1,script=$KVMDIR/lb-if-script/lb-ifup1,downscript=$KVMDIR/lb-if-script/lb-ifdown1,vlan=1 \\\n
-		-net nic,macaddr=00:00:00:00:11:$cur,vlan=2 \\\n
-		-net tap,vhost=on,id=vnic2,script=$KVMDIR/lb-if-script/lb-ifup2,downscript=$KVMDIR/lb-if-script/lb-ifdown2,vlan=2 \\\n
-		-display sdl -vga std \\" > $KVMDIR/linux-bridge-start/$cur.sh
+	  echo " sudo qemu-system-x86_64 -vnc :$cur \\
+		-enable-kvm -smp 8 -m 16384 \\
+		-drive file=$KVMDIR/image/image-$cur.img,if=virtio \\
+		-net nic,macaddr=00:00:00:00:10:$cur,vlan=1 \\
+		-net tap,vhost=on,id=vnic1,script=$KVMDIR/lb-if-script/lb-ifup1,downscript=$KVMDIR/lb-if-script/lb-ifdown1,vlan=1 \\
+		-net nic,macaddr=00:00:00:00:11:$cur,vlan=2 \\
+		-net tap,vhost=on,id=vnic2,script=$KVMDIR/lb-if-script/lb-ifup2,downscript=$KVMDIR/lb-if-script/lb-ifdown2,vlan=2 \\
+		-display sdl -vga std" > $KVMDIR/linux-bridge-start/$cur.sh
 
 	
   echo "start to mount VM image..."
@@ -101,11 +101,11 @@ do
   $QEMUDIR/qemu-nbd -d /dev/nbd0
 
 
-  echo "bash $cur.sh\n" >> /home/chix/kvm/linux-bridge-start/start-vm-all.sh
+  echo "bash $cur.sh" >> /home/chix/kvm/linux-bridge-start/start-vm-all.sh
 
-  echo "bash $cur.sh\n" >> /home/chix/kvm/ovs-dpdk-start/start-vm-all.sh
+  echo "bash $cur.sh" >> /home/chix/kvm/ovs-dpdk-start/start-vm-all.sh
   
-  echo "bash $cur.sh\n" >> /home/chix/kvm/ovs-nodpdk-start/start-vm-all.sh
+  echo "bash $cur.sh" >> /home/chix/kvm/ovs-nodpdk-start/start-vm-all.sh
 
 	
 #  for ((t=0; t<=0; t++))
